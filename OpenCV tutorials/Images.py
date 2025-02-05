c15ed1977733ca09e211d1c49be29392edb938f8
jadoratan <jadoratan@gmail.com>
2025-02-05 14:29:04 -0800

    images & videos

--- OpenCV tutorials/Images.py
+++ OpenCV tutorials/Images.py
@@ -1,7 +1,8 @@
 import cv2 as cv
 import sys
 
-img = cv.imread(cv.sampels.findFile("test.jpg"))
+########## Read Existing ###########
+img = cv.imread(cv.samples.findFile("test.jpg"))
 if img is None:
     sys.exit("Could not read the image.")
 
@@ -10,3 +11,42 @@ k = cv.waitKey(0)
 
 if k == ord("s"):
     cv.imwrite("test.jpg", img)
+
+
+########## Live Capture #############
+cap = cv.VideoCapture(0)
+
+# Check if the camera opened successfully
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+ret, frame = cap.read()
+
+if ret:
+    # Save the frame
+    cv.imwrite("test.jpg", frame)
+    print("Photo captured and saved as test.jpg.")
+else:
+    print("Error: Could not capture frame.")
+
+# Release the camera
+cap.release()
+
+im = cv.imread('test.jpg')
+assert im is not None, "file could not be read, check with os.path.exists()"
+# imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
+
+# Display the image in a window
+cv.imshow('Image Window', im)
+
+# Wait for a key press
+cv.waitKey(0)
+
+# Close the window
+cv.destroyAllWindows()
+
+# ret, thresh = cv.threshold(imgray, 127, 255, 0)
+# # print(cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE))
+# im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# cv.drawContours(im2, contours, -1, (0,255,0), 3)`
---
+++ OpenCV tutorials/Videos.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2 as cv
+
+
+######### Live Capture ###########
+cap = cv.VideoCapture(0)
+
+# RECORD: define the codec and create the VideoWriter object
+# fourcc = cv.VideoWriter_fourcc(*"XVID")
+# out = cv.VideoWriter("output.avi", fourcc, 20.0, (640, 480))
+
+# Check if the camera opened successfully
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+while True:
+    # Capture a frame
+    ret, frame = cap.read()
+
+    # Check if the frame was captured successfully
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting...")
+        break
+
+    # Operations on frame here
+    # frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # frame = cv.flip(frame, 0)
+    
+    # Display the resulting frame
+    cv.imshow('Image Window', frame)
+    
+    if cv.waitKey(1) == ord("q"):
+        break
+    
+# Release capture (RECORD: and out) when done
+cap.release()
+# out.release()
+cv.destroyAllWindows()
+
+# ret, thresh = cv.threshold(imgray, 127, 255, 0)
+# # print(cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE))
+# im2, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+# cv.drawContours(im2, contours, -1, (0,255,0), 3)`

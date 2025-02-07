e404bb0ead37a394d6de756d99f5793fd3fe0e24
jadoratan <jadoratan@gmail.com>
2025-02-07 14:25:14 -0800

    testing contours on a real test image

--- OpenCV tutorials/Contours.py
+++ OpenCV tutorials/Contours.py
@@ -5,10 +5,32 @@ from matplotlib import pyplot as plt
 # Contour hierarchy docs
 # https://docs.opencv.org/4.x/d9/d8b/tutorial_py_contours_hierarchy.html
 
-im = cv.imread("shapes.png")
-assert im is not None, "file could not be read, check with os.path.exists()"
+# im = cv.imread("shapes.png")
+# assert im is not None, "file could not be read, check with os.path.exists()"
+
+# Live Capture
+cap = cv.VideoCapture(0)
+
+# Check if the camera opened successfully
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+ret, frame = cap.read()
+
+if ret:
+    # Save the frame
+    cv.imwrite("test.jpg", frame)
+    print("Photo captured and saved as test.jpg.")
+else:
+    print("Error: Could not capture frame.")
+
+# Release the camera
+cap.release()
+
+im = cv.imread("test.jpg")
 imgray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
-ret, thresh = cv.threshold(imgray, 127, 255, cv.THRESH_BINARY)
+ret, thresh = cv.threshold(imgray, 100, 150, cv.THRESH_BINARY)
 
 temp = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 for i in range(len(temp)):

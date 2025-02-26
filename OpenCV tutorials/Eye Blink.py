dc0e4020a04a2940e128603a2246f93f8b1f874f
jadoratan <jadoratan@gmail.com>
2025-02-26 14:32:43 -0800

    blink text shows up
    but something is wrong with the logic that makes it keep displaying even when I don't blink

--- OpenCV tutorials/Eye Blink.py
+++ OpenCV tutorials/Eye Blink.py
@@ -5,7 +5,7 @@
 # Importing the required dependencies 
 import cv2 # for video rendering 
 import dlib # for face and landmark detection 
-
+import time
 import imutils 
 # for calculating dist b/w the eye landmarks 
 from scipy.spatial import distance as dist 
@@ -15,7 +15,13 @@ from imutils import face_utils
 
 # from imutils import 
 
-cam = cv2.VideoCapture('assets/my_blink.mp4') 
+cam = cv2.VideoCapture(0) 
+
+if not cam.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+else:
+	print("Camera opened! :D")
 
 # defining a function to calculate the EAR 
 def calculate_EAR(eye): 
@@ -32,9 +38,17 @@ def calculate_EAR(eye):
 	return EAR 
 
 # Variables 
-blink_thresh = 0.45
-succ_frame = 2
-count_frame = 0
+BLINK_THRESH = 0.45
+SUCC_FRAME = 2 # for preventing false detections from slight eye movement or noise
+
+left_count_frame = 0 # how many consecutive frames left eye is unblinking
+right_count_frame = 0 # how many consecutive frames right eye is unblinking
+
+BLINK_DISPLAY_FRAMES = 20 # Number of frames to display message
+left_blink_counter = 0 # Counter for displaying blink message
+
+# right_blink_display_frames = 10 # Number of frames to display message
+right_blink_counter = 0 # Counter for displaying blink message
 
 # Eye landmarks 
 (L_start, L_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"] 
@@ -43,15 +57,18 @@ count_frame = 0
 # Initializing the Models for Landmark and 
 # face Detection 
 detector = dlib.get_frontal_face_detector() 
-landmark_predict = dlib.shape_predictor( 
-	'Model/shape_predictor_68_face_landmarks.dat') 
-while 1: 
+landmark_predict = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat') 
+print("Initialized models for landmark and face detection")
 
+while 1: 
+	
 	# If the video is finished then reset it 
 	# to the start 
 	if cam.get(cv2.CAP_PROP_POS_FRAMES) == cam.get( 
 			cv2.CAP_PROP_FRAME_COUNT): 
 		cam.set(cv2.CAP_PROP_POS_FRAMES, 0) 
+
+	# print("hi")
 
 	else: 
 		_, frame = cam.read() 
@@ -77,20 +94,50 @@ while 1:
 			lefteye = shape[L_start: L_end] 
 			righteye = shape[R_start:R_end] 
 
-			# Calculate the EAR 
+			# Calculate the EAR (Eye Aspect Ratio)
 			left_EAR = calculate_EAR(lefteye) 
 			right_EAR = calculate_EAR(righteye) 
 
-			# Avg of left and right eye EAR 
-			avg = (left_EAR+right_EAR)/2
-			if avg < blink_thresh: 
-				count_frame += 1 # incrementing the frame count 
+			# Just left eye EAR
+			if (left_EAR < BLINK_THRESH): # if left eye is unblinking
+				left_count_frame += 1 # incrementing the frame count for left eye
 			else: 
-				if count_frame >= succ_frame: 
-					cv2.putText(frame, 'Blink Detected', (30, 30), 
-								cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1) 
+				if left_count_frame >= SUCC_FRAME: 
+					left_blink_counter = BLINK_DISPLAY_FRAMES
+					print("Left eye blink detected")
 				else: 
-					count_frame = 0
+					left_count_frame = 0
+			
+			if left_blink_counter > 0:
+				cv2.putText(frame, 'Left Eye Blink Detected', (30, 30), 
+								cv2.FONT_HERSHEY_PLAIN, 1, (0, 200, 0), 1) 
+				left_blink_counter -= 1
+
+			# Just right eye EAR
+			if (right_EAR < BLINK_THRESH): # if right eye is unblinking
+				right_count_frame += 1 # incrementing the frame count for right eye 
+			else: 
+				if right_count_frame >= SUCC_FRAME: 
+					right_blink_counter = BLINK_DISPLAY_FRAMES
+					print("Right eye blink detected")
+				else: 
+					right_count_frame = 0
+			
+			if right_blink_counter > 0:
+				cv2.putText(frame, 'Right Eye Blink Detected', (30, 230), 
+								cv2.FONT_HERSHEY_PLAIN, 1, (200, 0, 0), 1) 
+				right_blink_counter -= 1
+
+			# # Avg of left and right eye EAR 
+			# avg = (left_EAR+right_EAR)/2
+			# if avg < BLINK_THRESH: 
+			# 	left_count_frame += 1 # incrementing the frame count 
+			# else: 
+			# 	if left_count_frame >= SUCC_FRAME: 
+			# 		cv2.putText(frame, 'Blink Detected', (30, 30), 
+			# 					cv2.FONT_HERSHEY_DUPLEX, 1, (0, 200, 0), 1) 
+			# 	else: 
+			# 		left_count_frame = 0
 
 		cv2.imshow("Video", frame) 
 		if cv2.waitKey(5) & 0xFF == ord('q'): 

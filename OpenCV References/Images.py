902c0d5cd5d528b0ae5fa749fd486dd91a377785
jadoratan <jadoratan@gmail.com>
2025-03-25 13:36:02 -0700

    Update Images.py

--- OpenCV References/Images.py
+++ OpenCV References/Images.py
@@ -2,15 +2,15 @@ import cv2 as cv
 import sys
 
 ########## Read Existing ###########
-img = cv.imread(cv.samples.findFile("test.jpg"))
-if img is None:
-    sys.exit("Could not read the image.")
+# img = cv.imread(cv.samples.findFile("test.jpg"))
+# if img is None:
+#     sys.exit("Could not read the image.")
 
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
+# cv.imshow("Display window", img)
+# k = cv.waitKey(0)
 
-if k == ord("s"):
-    cv.imwrite("test.jpg", img)
+# if k == ord("s"):
+#     cv.imwrite("test.jpg", img)
 
 
 ########## Live Capture #############
